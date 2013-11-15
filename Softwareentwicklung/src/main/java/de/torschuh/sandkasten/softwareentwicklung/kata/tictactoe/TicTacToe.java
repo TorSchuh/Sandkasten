1bb6178296161d537de41aa9835ea76a6b237c44
TorSchuh <0beron@0beron.de>
2013-11-15 12:49:03 +0100

    add sonar

--- Softwareentwicklung/src/main/java/de/torschuh/sandkasten/softwareentwicklung/kata/tictactoe/TicTacToe.java
+++ Softwareentwicklung/src/main/java/de/torschuh/sandkasten/softwareentwicklung/kata/tictactoe/TicTacToe.java
@@ -1,16 +1,6 @@
 package de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe;
 
 import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.PlayboardTicTacToe;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldA1;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldA2;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldA3;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldB1;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldB2;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldB3;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldC1;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldC2;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldC3;
-import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.interfaces.Locatable;
 
 /**
  * TicTacToe game.
@@ -31,55 +21,11 @@ public class TicTacToe {
      * @param args Parameter for main method
      */
     public static void main(final String[] args) {
-        PlayboardTicTacToe playboard = (PlayboardTicTacToe) initializePlayboard();
-        printPlayboard(playboard);
+        PlayboardTicTacToe playboard = new PlayboardTicTacToe();
+        playboard.printPlayboard();
 //        do {
 //            endOfGame = nextMove(playboard); 
 //        } while (endOfGame);
-    }
-
-    /**
-     * Creates TicTacToe playboard.
-     * 
-     * @return playboard TicTacToe playboard
-     */
-    private static PlayboardTicTacToe initializePlayboard() {
-        PlayboardTicTacToe playboard = new PlayboardTicTacToe(); 
-            playboard.addField(new FieldA1());
-            playboard.addField(new FieldA2());
-            playboard.addField(new FieldA3());
-            playboard.addField(new FieldB1());
-            playboard.addField(new FieldB2());
-            playboard.addField(new FieldB3());
-            playboard.addField(new FieldC1());
-            playboard.addField(new FieldC2());
-            playboard.addField(new FieldC3());
-        return playboard;
-    }
-    
-    /**
-     * Prints out the actual playboard.
-     * 
-     * @param pPlayboard playboard of the TicTacToe game
-     */
-    private static void printPlayboard(final PlayboardTicTacToe pPlayboard) {
-        try {
-            System.out.println("     A       B       C");
-            System.out.println("");
-            System.out.println("1     " + pPlayboard.getField(Locatable.Identifier.A1).getToken().getType() + "  " 
-                                + "|" + pPlayboard.getField(Locatable.Identifier.B1).getToken().getType() + "      |  " 
-                                + pPlayboard.getField(Locatable.Identifier.C1).getToken().getType() + "  ");
-            System.out.println("   ----- + ----- + -----");
-            System.out.println("2     " + pPlayboard.getField(Locatable.Identifier.A2).getToken().getType() + "  "
-                                + "|" + pPlayboard.getField(Locatable.Identifier.B2).getToken().getType() + "      |  " 
-                                + pPlayboard.getField(Locatable.Identifier.C2).getToken().getType() + "  ");
-            System.out.println("   ----- + ----- + -----");
-            System.out.println("3     " + pPlayboard.getField(Locatable.Identifier.A3).getToken().getType() + "  "
-                                + "|" + pPlayboard.getField(Locatable.Identifier.B3).getToken().getType() + "      |  " 
-                                + pPlayboard.getField(Locatable.Identifier.C3).getToken().getType() + "  ");
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
     }
     
 }
--- Softwareentwicklung/src/main/java/de/torschuh/sandkasten/softwareentwicklung/kata/tictactoe/components/implementation/PlayboardTicTacToe.java
+++ Softwareentwicklung/src/main/java/de/torschuh/sandkasten/softwareentwicklung/kata/tictactoe/components/implementation/PlayboardTicTacToe.java
@@ -1,6 +1,16 @@
 package de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation;
 
 import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.abstraction.AbstractPlayboard;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldA1;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldA2;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldA3;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldB1;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldB2;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldB3;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldC1;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldC2;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.implementation.fields.FieldC3;
+import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.interfaces.Locatable;
 
 
 /**
@@ -10,10 +20,41 @@ import de.torschuh.sandkasten.softwareentwicklung.kata.tictactoe.components.abst
 public class PlayboardTicTacToe extends AbstractPlayboard {
 
     /**
-     * 
+     * Creates TicTacToe playboard withe field and blank tokens.
      */
     public PlayboardTicTacToe() {
-     
+        this.addField(new FieldA1());
+        this.addField(new FieldA2());
+        this.addField(new FieldA3());
+        this.addField(new FieldB1());
+        this.addField(new FieldB2());
+        this.addField(new FieldB3());
+        this.addField(new FieldC1());
+        this.addField(new FieldC2());
+        this.addField(new FieldC3());
+    }
+    
+    /**
+     * Prints out the actual playboard.
+     */
+    public final void printPlayboard() {
+        try {
+            System.out.println("     A       B       C");
+            System.out.println("");
+            System.out.println("1     " + this.getField(Locatable.Identifier.A1).getToken().getType() + "  " 
+                                + "|" + this.getField(Locatable.Identifier.B1).getToken().getType() + "      |  " 
+                                + this.getField(Locatable.Identifier.C1).getToken().getType() + "  ");
+            System.out.println("   ----- + ----- + -----");
+            System.out.println("2     " + this.getField(Locatable.Identifier.A2).getToken().getType() + "  "
+                                + "|" + this.getField(Locatable.Identifier.B2).getToken().getType() + "      |  " 
+                                + this.getField(Locatable.Identifier.C2).getToken().getType() + "  ");
+            System.out.println("   ----- + ----- + -----");
+            System.out.println("3     " + this.getField(Locatable.Identifier.A3).getToken().getType() + "  "
+                                + "|" + this.getField(Locatable.Identifier.B3).getToken().getType() + "      |  " 
+                                + this.getField(Locatable.Identifier.C3).getToken().getType() + "  ");
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
     
 }
